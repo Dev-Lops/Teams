030551951340c74635ba653a3a439bf2721d2590
Anderson Lopes <developes.anderson@gmail.com>
2024-07-30 09:56:24 -0400

    ✨ Estilização da statusbar

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { Loading } from '@components/Loading';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
 import theme from '@theme/index';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
 export default function App() {
@@ -9,10 +10,10 @@ export default function App() {
     Roboto_400Regular, Roboto_700Bold,
   })
 
-
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoader ? <Groups /> : <Loading />}
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontsLoader ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
 }
