0e3728d3446092639701cf32fe28512432772f58
Anderson Lopes <developes.anderson@gmail.com>
2024-07-30 10:47:23 -0400

    ✨ Component Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,14 @@
 import { Header } from '@components/Header';
 import * as S from './styles';
+import { Highlight } from '@components/Highlight';
 
 export function Groups() {
   return (
     <S.Container>
       <Header />
+      <Highlight
+        title='Equipes'
+        subtitle='Forme sua equipe e divirta-se!' />
     </S.Container>
   );
 }
