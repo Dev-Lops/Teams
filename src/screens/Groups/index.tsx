33b25a211a1cb337c8622eed8dce6caf5a5c78d6
Anderson Lopes <developes.anderson@gmail.com>
2024-07-30 11:01:36 -0400

    ✨ Componente Group Card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@components/Header';
 import * as S from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
@@ -9,6 +10,8 @@ export function Groups() {
       <Highlight
         title='Equipes'
         subtitle='Forme sua equipe e divirta-se!' />
+
+      <GroupCard title='Vale de Benção' />
     </S.Container>
   );
 }
