b393da6441f602ead18bb2548a340f158ba60a9c
Anderson Lopes <developes.anderson@gmail.com>
2024-07-30 02:58:06 -0400

    ✨ Alias de importação

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,13 +1,13 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  flex: 1;
-  background-color: #000;
-  align-items: center;
-  justify-content: center;
+	flex: 1;
+	background-color: #000;
+	align-items: center;
+	justify-content: center;
 `;
 
 export const Title = styled.Text`
-  color: #fff;
-  font-size: 32px;
+	color: #fff;
+	font-size: 32px;
 `;
