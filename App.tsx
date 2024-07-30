018df37def55368134fa0edad7e5e7ac3a7cb34e
Anderson Lopes <developes.anderson@gmail.com>
2024-07-30 09:43:54 -0400

    ✨ Instalando as fontes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
 import theme from '@theme/index';
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
 export default function App() {
+  const [fontsLoader] = useFonts({
+    Roboto_400Regular, Roboto_700Bold,
+  })
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoader ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }
