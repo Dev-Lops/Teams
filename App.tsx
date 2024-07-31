128a12e47bca8d56237013c933bac80b08026ab6
Anderson Lopes <developes.anderson@gmail.com>
2024-07-31 00:04:59 -0400

    ✨ Implementando Stack navigator

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { Loading } from '@components/Loading';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { NavigationContainer } from '@react-navigation/native';
 import { Groups } from '@screens/Groups';
 import { NewGroup } from '@screens/NewGroup';
 import { Players } from '@screens/Players';
@@ -14,11 +13,10 @@ export default function App() {
   })
 
   return (
-    <NavigationContainer>
-      <ThemeProvider theme={theme}>
-        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-        {fontsLoader ? <Groups /> : <Loading />}
-      </ThemeProvider>
-    </NavigationContainer>
+
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontsLoader ? <Groups /> : <Loading />}
+    </ThemeProvider>
   )
 }
