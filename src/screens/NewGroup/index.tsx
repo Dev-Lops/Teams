f7192aa4a8018ffd0169c59e7a417c52760d4bbc
Anderson Lopes <developes.anderson@gmail.com>
2024-07-31 15:47:17 -0400

    ✨ Tratando exceções

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,12 +1,14 @@
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
-import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const [group, setGroup] = useState('')
@@ -15,11 +17,20 @@ export function NewGroup() {
 
   async function handleNewGroup() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Nome do grupo é obrigatório')
+      }
       await groupCreate(group)
       navigation.navigate('players', { group })
 
     } catch (error) {
-      console.error(error)
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Houve um erro ao tentar criar o grupo')
+        console.error(error)
+      }
+
     }
   }
 
